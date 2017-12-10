187214ef762d95b243c53bf8f8cc9b3fd7f008c1
Anup Dhamala <adhamala@wayfair.com>
2017-12-09 21:09:40 -0500

    setup kafka client connection

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/Shopify/sarama"
+)
+
+func failf(msg string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, msg+"\n", args...)
+	os.Exit(1)
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	//fmt.Println("Hello, world")
+
+	//bootstrapBrokers := "kafkac1n1.dev.bo1.csnzoo.com:9092,kafkac1n2.dev.bo1.csnzoo.com:9092,kafkac1n3.dev.bo1.csnzoo.com:9092"
+	bootstrapBrokers := []string{
+		"kafkac1n1.dev.bo1.csnzoo.com:9092",
+		"kafkac1n2.dev.bo1.csnzoo.com:9092",
+		"kafkac1n3.dev.bo1.csnzoo.com:9092",
+	}
+
+	config := sarama.NewConfig()
+	//config.Version = 'placeholder'
+	//config.ClientID = 'wf_kafka_lag_exporter'
+
+	client, err := sarama.NewClient(bootstrapBrokers, config)
+	if err != nil {
+		failf("failed to create client err=%v", err)
+	}
+
+	brokers := client.Brokers()
+	fmt.Fprintf(os.Stderr, "found %v brokers\n", len(brokers))
 }
